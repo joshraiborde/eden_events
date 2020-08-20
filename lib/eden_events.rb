285840eca915649011d48194ea2f85e604a33ff3
Josh Raiborde <joshsrai@yahoo.com>
2020-08-20 13:44:28 +0000

    2020-08-20

--- lib/eden_events.rb
+++ lib/eden_events.rb
@@ -1,4 +1,10 @@
-require "eden_events/version"
+require_relative "./eden_events/version"
+require_relative "./eden_events/cli"
+require_relative "./eden_events/month"
+require_relative "./eden_events/scraper"
+
+require 'pry'
+require 'nokogiri'
 
 module EdenEvents
   class Error < StandardError; end
